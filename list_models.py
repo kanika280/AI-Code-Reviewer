4c153f7a6d14353cc27d673490235117092ca58e
kanika280 <53091692+kanika280@users.noreply.github.com>
2026-01-07 18:39:13 +0100

    Update list_models.py

--- list_models.py
+++ list_models.py
@@ -2,8 +2,9 @@ from groq import Groq
 import os
 
 # Make sure your API key is set in this terminal
-client = Groq(api_key=os.getenv("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"))
+client = Groq(api_key=os.getenv("Groq_API_KEY"))
 
 # List all models your key has access to
 for model in client.models.list():
     print(model)
+
